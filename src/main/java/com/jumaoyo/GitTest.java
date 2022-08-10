6622f3f582e863b236da9d9f01841cc44b313b24
jumaoyo <jumaoyo@126.com>
2022-08-11 00:22:51 +0800

    first commit

---
+++ src/main/java/com/jumaoyo/GitTest.java
@@ -0,0 +1,13 @@
+package com.jumaoyo;
+
+/**
+ * @description <TODO description class purpose>
+ * @Author jumaoyo
+ * @Date 2022/8/11 00:19
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
